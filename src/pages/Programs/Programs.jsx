803d1abf675f565bd04e018f2878e19a1bb998c5
raunakbiz <raunak@india100x.com>
2024-12-05 11:13:13 +0530

    program/program-chart/program-programtable

--- src/pages/Programs/Programs.jsx
+++ src/pages/Programs/Programs.jsx
@@ -1,12 +1,15 @@
+import { ProgramChart } from "../../components/program-chart"
+import { DataTableDemo } from "../../components/program-table"
+
 const Programs = () => {
   return (
     <div className="flex flex-col gap-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
+         <ProgramChart className="aspect-video rounded-xl bg-muted/50"/>
+         <ProgramChart className="aspect-video rounded-xl bg-muted/50"/>
+         <ProgramChart className="aspect-video rounded-xl bg-muted/50"/>
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      <DataTableDemo className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
     </div>
   )
 }
