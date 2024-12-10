153c50522163f7abb5b43748ae1840aa2447296b
mitsuxhoney <hiveorxythm@gmail.com>
2024-12-10 14:11:44 +0530

    updated code

--- src/components/ui/team-logs-table.jsx
+++ src/components/ui/team-logs-table.jsx
@@ -132,9 +132,7 @@ export function TeamLogsTable() {
     },
     {
       accessorKey: 'event',
-      header: ({ column }) => {
-        return <Button variant="ghost">Event</Button>
-      },
+      header: 'Event',
       cell: ({ row }) => (
         <div className="capitalize pl-4">{row.getValue('event')}</div>
       ),
@@ -142,17 +140,7 @@ export function TeamLogsTable() {
 
     {
       accessorKey: 'date',
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-          >
-            Date
-            <ArrowUpDown />
-          </Button>
-        )
-      },
+      header: 'Date',
       cell: ({ row }) => (
         <div className="lowercase pl-4">{row.getValue('date')}</div>
       ),
@@ -224,21 +212,26 @@ export function TeamLogsTable() {
                 <DropdownMenuContent align="end">
                   {table
                     .getAllColumns()
-                    .filter((column) => column.getCanHide())
-                    .map((column) => {
-                      return (
-                        <DropdownMenuCheckboxItem
-                          key={column.id}
-                          className="capitalize"
-                          checked={column.getIsVisible()}
-                          onCheckedChange={(value) =>
-                            column.toggleVisibility(!!value)
-                          }
-                        >
-                          {column.id}
-                        </DropdownMenuCheckboxItem>
-                      )
-                    })}
+                    .filter(
+                      (column) =>
+                        column.getCanHide() && // Check if the column can be hidden
+                        column.columnDef.header // Ensure the column has a defined header
+                    )
+                    .map((column) => (
+                      <DropdownMenuCheckboxItem
+                        key={column.id}
+                        className="capitalize"
+                        checked={column.getIsVisible()}
+                        onCheckedChange={(value) =>
+                          column.toggleVisibility(!!value)
+                        }
+                      >
+                        {typeof column.columnDef.header === 'string'
+                          ? column.columnDef.header
+                          : ''}{' '}
+                        {/* Render the header if it's a string */}
+                      </DropdownMenuCheckboxItem>
+                    ))}
                 </DropdownMenuContent>
               </DropdownMenu>
             </div>
